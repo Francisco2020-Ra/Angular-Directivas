319354921f84f2324d325a580fc0497a3d2f68ee
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-10-18 11:09:31 -0300

    Directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivas';
+  titulo: string = 'Registro de Usuarios';
+  mensaje: string ="";
+
+  nombre:string = "";
+  apellido:string = "";  
+
+  entradas: Array<{titulo:string}>;
+
+  constructor(){
+    this.entradas=[
+
+      {titulo:"Python cada día más presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"JavaScript cada vez mas funcional"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"¿Dónde quedó pascal?"},
+    ]
+  }
+  registrado:boolean = false;
+
+  registrarUsuario(){
+    this.registrado = true;
+    this.mensaje = "Usuario registrado con exito"
+  }
 }
